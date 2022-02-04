f3fef097e7920c803b83f24f7836b8cb973f0075
Dejan Totev <dejan.totev@psinteractive.io>
2022-02-04 18:33:19 +0200

    feat: introduce forbidden extensions

--- packages/magic-beans/src/randomFile.js
+++ packages/magic-beans/src/randomFile.js
@@ -7,15 +7,20 @@ const {
   random,
   removeIndex,
 } = require('rambdax')
-const { configAnt } = require('./ants/config')
-const { logToUser } = require('./bar')
-const { REQUEST_RANDOM_FILE } = require('./constants')
-const { scanFolder } = require('helpers-fn')
+const {configAnt} = require('./ants/config')
+const {logToUser} = require('./bar')
+const {REQUEST_RANDOM_FILE} = require('./constants')
+const {scanFolder} = require('helpers-fn')
 
 const RANDOM_FILE_SKIP_PATTERNS = configAnt('RANDOM_FILE_SKIP_PATTERNS')
-const RANDOM_FILE_ALLOWED_EXTENSIONS = configAnt('RANDOM_FILE_ALLOWED_EXTENSIONS')
+const RANDOM_FILE_ALLOWED_EXTENSIONS = configAnt(
+  'RANDOM_FILE_ALLOWED_EXTENSIONS'
+)
+const RANDOM_FILE_FORBIDDEN_EXTENSIONS = configAnt(
+  'RANDOM_FILE_FORBIDDEN_EXTENSIONS'
+)
 
-function changeOpenedFile(filePath, callback = () => {}){
+function changeOpenedFile(filePath, callback = () => {}) {
   // editor should have
   // "workbench.editor.enablePreview": true,
   const openPath = vscode.Uri.file(filePath)
@@ -25,28 +30,39 @@ function changeOpenedFile(filePath, callback = () => {}){
   })
 }
 
-function requestRandomFile(){
+function requestRandomFile() {
   const files = getter('files')
   if (files.length === 0) return
   const index = random(0, files.length - 1)
-  changeOpenedFile(files[ index ])
-  setter('files', removeIndex(files, index))
-  logToUser(`${ files.length - 1 } files left`)
+  changeOpenedFile(files[index])
+  setter('files', removeIndex(index, files))
+  logToUser(`${files.length - 1} files left`)
 }
 
-async function randomFile(){
+async function randomFile() {
   if (getter(REQUEST_RANDOM_FILE)) return
   setter(REQUEST_RANDOM_FILE, true)
 
-  const projectFolder = vscode.workspace.workspaceFolders[ 0 ].uri.path
+  const projectFolder = vscode.workspace.workspaceFolders[0].uri.path
   const files = await scanFolder({
-    folder    : projectFolder,
-    maxDepth  : 20,
-    excludeFn : dir => RANDOM_FILE_SKIP_PATTERNS.includes(dir),
-    filterFn  : filePath => {
-      const [ pass ] = RANDOM_FILE_ALLOWED_EXTENSIONS.filter(singleExtension => filePath.endsWith(singleExtension))
+    folder: projectFolder,
+    maxDepth: 20,
+    excludeFn: dir => RANDOM_FILE_SKIP_PATTERNS.includes(dir),
+    filterFn: filePath => {
+      const [
+        passAllowedExtension,
+      ] = RANDOM_FILE_ALLOWED_EXTENSIONS.filter(singleExtension =>
+        filePath.endsWith(singleExtension)
+      )
+      if (!passAllowedExtension) return false
 
-      return Boolean(pass)
+      const [
+        failForbiddenExtension,
+      ] = RANDOM_FILE_FORBIDDEN_EXTENSIONS.filter(singleExtension =>
+        filePath.endsWith(singleExtension)
+      )
+
+      return Boolean(failForbiddenExtension) ? false : true
     },
   })
 
