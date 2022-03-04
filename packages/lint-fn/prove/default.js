552f4385195544dfe90dfd13fd097fe61a78a43f
Deyan Totev <deyan.totev.extern@seven.one>
2022-03-04 20:35:49 +0200

    chore@dnd

--- packages/lint-fn/prove/default.js
+++ packages/lint-fn/prove/default.js
@@ -2,7 +2,7 @@ process.env.LINT_FN_DEBUG = 'ON'
 process.env.SKIP_ESLINT_RULES = 'no-nested-ternary,max-len'
 const { lintFn } = require('../lintFn')
 
-const filePath = `${ process.env.HOME }/repos/rambda/source/lensPath.js`
+const filePath = `${ process.env.HOME }/repos/rambda/source/default.spec.js`
 
 void (async function prove(){
   console.time('prove')
