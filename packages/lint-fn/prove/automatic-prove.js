74859bb3f4edf46378f9ebb6776ab23eb194aa72
Deyan Totev <deyan.totev.extern@seven.one>
2022-07-31 10:44:46 +0300

    feat: complete auto prove

--- packages/lint-fn/prove/automatic-prove.js
+++ packages/lint-fn/prove/automatic-prove.js
@@ -1,23 +1,37 @@
 NODE_ENV = 'DEBUG'
 process.env.LINT_FN_DEBUG = 'ON'
 const { test,replace,mapAsync, mapFastAsync } = require('rambdax')
-const { readFile } = require('fs-extra')
+const { readFile, writeFile } = require('fs-extra')
 const { lintFn, execPrettier,  } = require('../lintFn')
 const { ANGULAR, TS_PROVE, JS, TS, JEST, ANGULAR_HTML } = require('../constants')
 
-const commonReplacer = (fileContent) => {
-  console.log(fileContent, `fileContent`)
-  const havePattern = test(/\t/, fileContent)
-  console.log(havePattern, `havePattern`)
+const commonReplacer = (fileContent, {replacer}) => {
+  console.log(replacer, `replacer`)
+  const havePattern = fileContent.includes(replacer.old)
+  if(!havePattern) return false
+
+  return replace(replacer.old, replacer.new,fileContent)
 }
 
-async function singleProve(lintFnInput, replacer = commonReplacer){
-  const fileContent = (await readFile(lintFnInput.filePath)).toString()
-  const newContent = commonReplacer(fileContent)
+async function singleProve(lintFnInput){
+  const initialFileContent = (await readFile(lintFnInput.filePath)).toString()
+  const newContent = commonReplacer(initialFileContent, lintFnInput)
+  if(!newContent) return {linted: false, mode: lintFnInput.mode, error: 'wrong replacer'}
+
+  await writeFile(lintFnInput.filePath, newContent)
+  const lintResult = await lintFn(lintFnInput)
+
+  const lintedFileContent = (await readFile(lintFnInput.filePath)).toString()
+
+  if(initialFileContent !== lintedFileContent){
+    return {linted: false, mode: lintFnInput.mode, error: 'unexpected change'}
+  }
+
+  return {linted: true, mode: lintFnInput.mode, lintResult}
 }
 
 const proveList = [
-  {filePath: ANGULAR}
+  {filePath: ANGULAR, mode: 'angular', replacer: {old: '@Component({', new: `   @Component({`}}
 ]
 
 void async function automaticProve(){
