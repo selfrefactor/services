60bdcf6003f7943023a042404b6e2d6c5dbe6505
Deyan Totev <deyan.totev.extern@seven.one>
2022-08-14 14:42:59 +0300

    chore@bump patch

--- packages/run-fn/services/clone/clone.js
+++ packages/run-fn/services/clone/clone.js
@@ -1,15 +1,17 @@
 const {execCommand} = require('../../modules/execCommand')
 const {existsSync} = require('fs')
 const {log} = require('helpers-fn')
-const { piped, split, last, remove } = require('rambdax')
+const { piped, split, last } = require('rambdax')
 
 async function clone(repoInput) {
-  const repo = repoInput.startsWith('git@github.com') ? repoInput : 
-    `git@github.com:selfrefactor/${repoInput}.git`
-  const repoName = piped(repo, split(':'), last, remove('.git'))  
+  const flag = repoInput.startsWith('https://github.com/')
+  const repoID = flag ? piped(repoInput, split('https://github.com/'), last) : `selfrefactor/${repoInput}`
+  const [,repoName] = repoID.split(`/`)
 
-  console.log(`git clone ${repo}`, repoName)
-  await execCommand(`git clone ${repo}`)
+  const command = `git clone git@github.com:${repoID}.git` 
+  await execCommand(flag ? `${command} --depth 1`: command)
+  console.log(flag ? `${command} --depth 1`: command)
+
   const maybePackageJson = `${process.cwd()}/${repoName}/package.json`
   if (!existsSync(maybePackageJson)) {
     return log('No package json found, will skip install process', 'info')
@@ -18,7 +20,7 @@ async function clone(repoInput) {
   const dependencyInstaller = existsSync(maybePackageLock) ? 'npm' : 'yarn'
   await execCommand(
     `${dependencyInstaller} install`,
-    `${process.cwd()}/${repo}`
+    `${process.cwd()}/${repoName}`
   )
 }
 
