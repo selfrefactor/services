7d127f6b8c91faab6404ec48f128756c56af9272
Deyan Totev <deyan.totev.extern@seven.one>
2022-08-16 14:10:29 +0300

    feat@important slow scroll

--- packages/magic-beans/src/extension.js
+++ packages/magic-beans/src/extension.js
@@ -7,6 +7,7 @@ const { initBar } = require('./bar')
 const { requestRandomFile } = require('./randomFile')
 const { REQUEST_RANDOM_FILE, SORT_LINES } = require('./constants')
 const { sortLines } = require('./sort-lines')
+const { slowScroll } = require('./slow-scroll')
 
 function activate(context){
   initBar()
@@ -14,8 +15,9 @@ function activate(context){
     formatJson)
   const copyTrimmedCommand = vscode.commands.registerCommand('magicBeans.copyTrimmed',
     copyTrimmed)
-  const createSpecCommand = vscode.commands.registerCommand('magicBeans.createSpec',
-    createSpec)
+  const createSpecCommand = vscode.commands.registerCommand('magicBeans.createSpec',createSpec)
+  const slowScrollCommand = vscode.commands.registerCommand('magicBeans.slowScroll',
+    slowScroll(context))
   const requestRandomFileCommand = vscode.commands.registerCommand(REQUEST_RANDOM_FILE,
     requestRandomFile)
   const sortLinesCommand = vscode.commands.registerCommand(SORT_LINES,
@@ -25,6 +27,7 @@ function activate(context){
 
   context.subscriptions.push(copyTrimmedCommand)
   context.subscriptions.push(createSpecCommand)
+  context.subscriptions.push(slowScrollCommand)
   context.subscriptions.push(requestRandomFileCommand)
   context.subscriptions.push(formatJsonCommand)
   context.subscriptions.push(sortLinesCommand)
---
+++ packages/magic-beans/src/slow-scroll.js
@@ -0,0 +1,111 @@
+// taken from https://github.com/windwp/scroll_auto
+const vscode = require('vscode')
+const { configAnt } = require('./ants/config')
+
+const STEP = configAnt(
+  'SLOW_SCROLL'
+)
+
+class ContextKey{
+  constructor(name){
+    this._name = name
+  }
+
+  set(value){
+    if (this._lastValue === value){
+      return
+    }
+    this._lastValue = value
+    vscode.commands.executeCommand(
+      'setContext', this._name, this._lastValue
+    )
+  }
+}
+
+class ScrollController{
+  dispose(){
+    this.stopScroll()
+    this._disposable.dispose()
+  }
+
+  constructor(){
+    this.isAutoScroll = new ContextKey('scroll.isStart')
+    const subscriptions = []
+    this._disposable = vscode.Disposable.from(...subscriptions)
+  }
+
+  startScroll(line){
+    this._line = line
+    if (this._isRunning){
+      return
+    }
+    this._isRunning = true
+    this.isAutoScroll.set(this._isRunning)
+    this.scrollInterval = setInterval(() => {
+      const result = this.scroll(this._line)
+      if (!result){
+        this.stopScroll()
+      }
+    }, STEP ?? 300)
+  }
+
+  scroll(line){
+    const editor = vscode.window.activeTextEditor
+    if (!editor){
+      return false
+    }
+    const currentPosition = editor.selection.active
+    let moveToLine = currentPosition.line + line
+    const documentLineCount = editor.document.lineCount
+    if (moveToLine > documentLineCount - 1){
+      moveToLine = documentLineCount - 1
+
+      return false
+    }
+    if (moveToLine < 0){
+      moveToLine = 0
+
+      return false
+    }
+    const moveToCharactor =
+      editor.document.lineAt(moveToLine).firstNonWhitespaceCharacterIndex
+    const newPosition = new vscode.Position(moveToLine, moveToCharactor)
+    editor.selection = new vscode.Selection(newPosition, newPosition)
+    editor.revealRange(editor.selection,
+      vscode.TextEditorRevealType.InCenter)
+
+    return true
+  }
+
+  stopScroll(){
+    if (this._isRunning){
+      this._isRunning = false
+      if (this.scrollInterval){
+        this.isAutoScroll.set(false)
+        clearInterval(this.scrollInterval)
+      }
+    }
+  }
+}
+
+function slowScroll(context){
+  return () => {
+    const controller = new ScrollController()
+
+    const stopHandler = vscode.commands.registerCommand('magicBeans.scroll_auto_stop',
+      () => {
+        controller.stopScroll()
+      })
+
+    const downHandler = vscode.commands.registerCommand('magicBeans.scroll_auto_start',
+      () => {
+        controller.startScroll(1)
+      })
+    context.subscriptions.push(downHandler)
+    context.subscriptions.push(stopHandler)
+    context.subscriptions.push(controller)
+    controller.startScroll(1)
+  }
+}
+
+exports.slowScroll = slowScroll
