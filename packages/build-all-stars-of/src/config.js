95dd2d889329edc7d4d22a7474a9830fbcc967c4
Deyan Totev <deyan.totev.extern@seven.one>
2022-08-18 16:52:28 +0300

    chore@small

--- packages/build-all-stars-of/src/config.js
+++ packages/build-all-stars-of/src/config.js
@@ -1,5 +1,5 @@
-const { resolve } = require('path')
 const { map } = require('rambdax')
+const { resolve } = require('path')
 const BASE = resolve(__dirname, '../../all-stars-of/')
 
 const shouldRefreshScraped = true
@@ -9,99 +9,114 @@ const SHOW_PROGRESS = 1
 const MAX_SCRAPE_DEPTH = 600
 
 const playwright = {
-  priority: 0,
-  daysLimit: 150,
-  repo: 'microsoft/playwright',
-  title : 'Popular dependants of **Playwright**',
-  stars:5
+  priority  : 0,
+  daysLimit : 150,
+  repo      : 'microsoft/playwright',
+  title     : 'Popular dependents of **Playwright**',
+  stars     : 5,
 }
 const puppeteer = {
-  priority: 2,
-  daysLimit: 150,
-  repo: 'puppeteer/puppeteer',
-  title : 'Popular dependants of **Puppeteer**',
-  stars:10
+  priority  : 2,
+  daysLimit : 150,
+  repo      : 'puppeteer/puppeteer',
+  title     : 'Popular dependents of **Puppeteer**',
+  stars     : 10,
 }
 const nest = {
-  priority: 2,
-  daysLimit: 200,
-  repo: 'nestjs/nest',
-  title : 'Popular dependants of **Nest.js**',
-  stars:10
+  priority  : 2,
+  daysLimit : 200,
+  repo      : 'nestjs/nest',
+  title     : 'Popular dependents of **Nest.js**',
+  stars     : 10,
 }
 const angular = {
-  priority: 0,
-  daysLimit: 120,
-  scrapeDeep: true,
-  repo: 'angular/angular',
-  title : 'Popular dependants of **Angular** frontend framework',
-  stars:10
+  priority   : 0,
+  daysLimit  : 120,
+  scrapeDeep : true,
+  repo       : 'angular/angular',
+  title      : 'Popular dependents of **Angular** frontend framework',
+  stars      : 10,
 }
 
 const rambda = {
-  priority: 1,
-  scrapeDeep: true,
-  repo: 'selfrefactor/rambda',
-  title : 'Popular dependants of **Rambda** list',
-  stars: 2
+  priority   : 1,
+  scrapeDeep : true,
+  repo       : 'selfrefactor/rambda',
+  title      : 'Popular dependents of **Rambda**',
+  stars      : 2,
+}
+
+const rambdax = {
+  priority   : 1,
+  scrapeDeep : true,
+  repo       : 'selfrefactor/rambdax',
+  title      : 'Popular dependents of **Rambdax**',
+  stars      : 2,
 }
 
 const swc = {
-  priority: 2,
-  repo: 'swc-project/swc',
-  title : 'Popular dependants of **SWC(Rust-based platform for the Web)** list',
-  stars: 10
+  priority : 2,
+  repo     : 'swc-project/swc',
+  title    : 'Popular dependents of **SWC(Rust-based platform for the Web)**',
+  stars    : 10,
 }
 
 const ramda = {
-  priority: 1,
-  repo: 'ramda/ramda',
-  title : 'Popular dependants of **Ramda** list',
-  stars: 2
+  priority : 1,
+  repo     : 'ramda/ramda',
+  title    : 'Popular dependents of **Ramda**',
+  stars    : 2,
 }
 
 const vitest = {
-  priority: 1,
-  repo: 'vitest-dev/vitest',
-  title : 'Popular dependants of **Vitest** list',
-  stars: 2
+  priority : 1,
+  repo     : 'vitest-dev/vitest',
+  title    : 'Popular dependents of **Vitest**',
+  stars    : 2,
 }
 
 const cucumber = {
-  priority: 1,
-  repo: 'cucumber/cucumber-js',
-  scrapeDeep: true,
-  title : 'Popular dependants of **Cucumber.js** list',
-  stars: 2
+  priority   : 1,
+  repo       : 'cucumber/cucumber-js',
+  scrapeDeep : true,
+  title      : 'Popular dependents of **Cucumber.js**',
+  stars      : 2,
 }
 
 const kefir = {
-  priority: 2,
-  repo: 'kefirjs/kefir',
-  scrapeDeep: true,
-  title : 'Popular dependants of **Kefir** list',
-  stars: 2
+  priority   : 2,
+  repo       : 'kefirjs/kefir',
+  scrapeDeep : true,
+  title      : 'Popular dependents of **Kefir**',
+  stars      : 2,
 }
 
-const iterator = (x, prop) => {
-  return {
-    ...x,
-    daysLimit: x.daysLimit ? x.daysLimit : 370,
-    isHuge: x.isHuge ? x.isHuge : true,
-    scrapeDeep: x.scrapeDeep ? x.scrapeDeep : false,
-    showProgress: SHOW_PROGRESS,
-    maxScrapeDepth: MAX_SCRAPE_DEPTH,
-    isDev: IS_DEV,
-    starsLimit: x.stars ? x.stars : 5,
-    shouldRefreshApi,
-    shouldRefreshScraped,
-    outputLocation: `${BASE}/stars-of-${prop}.md`
-  }
-}
+const iterator = (x, prop) => ({
+  ...x,
+  daysLimit      : x.daysLimit ? x.daysLimit : 370,
+  isHuge         : x.isHuge ? x.isHuge : true,
+  scrapeDeep     : x.scrapeDeep ? x.scrapeDeep : false,
+  showProgress   : SHOW_PROGRESS,
+  maxScrapeDepth : MAX_SCRAPE_DEPTH,
+  isDev          : IS_DEV,
+  starsLimit     : x.stars ? x.stars : 5,
+  shouldRefreshApi,
+  shouldRefreshScraped,
+  outputLocation : `${ BASE }/stars-of-${ prop }.md`,
+})
 
-const allModes = map(
-  iterator,
-  {rambda, playwright, puppeteer, ramda, nest, angular, swc, vitest, cucumber, kefir}
-)
+const allModes = map(iterator, {
+  rambda,
+  rambdax,
+  playwright,
+  puppeteer,
+  ramda,
+  nest,
+  angular,
+  swc,
+  vitest,
+  cucumber,
+  kefir,
+})
 
 exports.allModes = allModes
