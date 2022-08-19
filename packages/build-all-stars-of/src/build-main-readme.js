d438e042238719dd0ba82bdbe5e95d54330a1f28
Deyan Totev <deyan.totev.extern@seven.one>
2022-08-19 10:26:23 +0300

    feat: connect to other repo

--- packages/build-all-stars-of/src/build-main-readme.js
+++ packages/build-all-stars-of/src/build-main-readme.js
@@ -13,16 +13,16 @@ const {
 } = require('rambdax')
 const { allModes } = require('./config')
 const { existsSync } = require('fs')
-const { resolve } = require('path')
+const { pascalCase } = require('string-fn')
 const { readFile, writeFile } = require('fs-extra')
-const { pascalCase} = require('string-fn')
+const { resolve } = require('path')
 
 void (async function main(){
   const iterator = async prop => {
     const { outputLocation, repo } = allModes[ prop ]
-    const [ ,titleRaw ] = repo.split('/')
+    const [ , titleRaw ] = repo.split('/')
     const title = pascalCase(titleRaw)
-    if(!existsSync(outputLocation)) return {skip:true}
+    if (!existsSync(outputLocation)) return { skip : true }
     const content = (await readFile(outputLocation)).toString()
     let total
     const preview = piped(
@@ -31,11 +31,15 @@ void (async function main(){
       drop(1),
       tap(list => total = list.length),
       take(5),
-      prepend(`## ${title}\n\n`),
-      join('###'),
+      prepend(`## ${ title }\n\n`),
+      join('###')
     )
 
-    return {preview, title, toc: `* [${title}](https://github.com/selfrefactor/services/blob/master/packages/all-stars-of/stars-of-${title.toLowerCase()}.md) - list of ${total} repos`}
+    return {
+      preview,
+      title,
+      toc : `* [${ title }](https://github.com/selfrefactor/popular-dependents-lists/blob/master/dependents-of-${ title.toLowerCase() }.md) - list of ${ total } repos`,
+    }
   }
   const sortedKeys = Object.keys(allModes).sort((a, b) => {
     if (allModes[ a ].priority === allModes[ b ].priority){
@@ -48,23 +52,32 @@ void (async function main(){
     return allModes[ a ].priority > allModes[ b ].priority ? 1 : -1
   })
   const parsed = await mapAsync(iterator, sortedKeys)
-  const filtered = parsed.filter(({skip}) => !skip)
+  const filtered = parsed.filter(({ skip }) => !skip)
+  const buildInfo = `
+All these lists are build with **Playwright**. Suggestions are welcome for adding more repos to the list. 
+`.trim()
 
   const template = `
-# List with all **Stars of** lists
+# List with all **Popular dependents** lists
 
 {{tableOfContents}}
 
 > What follows are the top 5 repos for each of the lists
 
 {{previews}}
-`.trim()
-  const tableOfContents = filtered.map(({toc}) => toc).join('\n')
-  const previews = filtered.map(({preview}) => preview).join('---\n\n')
-  const finalContent = interpolate(template, {tableOfContents, previews})
 
-  await writeFile(
-    resolve(__dirname, '../../all-stars-of/README.md'),
-    finalContent
-  )
+---
+
+{{buildInfo}}
+`.trim()
+  const tableOfContents = filtered.map(({ toc }) => toc).join('\n')
+  const previews = filtered.map(({ preview }) => preview).join('---\n\n')
+  const finalContent = interpolate(template, {
+    tableOfContents,
+    previews,
+    buildInfo,
+  })
+
+  await writeFile(resolve(__dirname, '../../all-stars-of/README.md'),
+    finalContent)
 })()
