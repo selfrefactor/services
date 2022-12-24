377302da2c531d1f02837a7a24e9e02dd472c963
Deyan Totev <deyan.totev.extern@seven.one>
2022-12-24 11:27:00 +0200

    chore@small

--- packages/vscode-control/src/syncSettings.js
+++ packages/vscode-control/src/syncSettings.js
@@ -33,8 +33,9 @@ const SNIPPETS_SOURCE = resolve(__dirname, '../.vscode/snippets.json')
 // const DEFAULT_FONT = 'Hack'
 // const DEFAULT_FONT = 'Fira Mono'
 // const DEFAULT_FONT = 'Cascadia Mono'
-const DEFAULT_FONT = 'JetBrains Mono'
+// const DEFAULT_FONT = 'JetBrains Mono'
 // const DEFAULT_FONT = 'Ubuntu Mono'
+const DEFAULT_FONT = 'Monoid Mono'
 
 function syncFiles(source, destination){
   copySync(source, destination)
