f78935d0165636db8f8fd8f6f2615b5fe39f1a07
Deyan Totev <deyan.totev.extern@seven.one>
2023-01-03 12:05:04 +0200

    chore@small

--- packages/vscode-control/src/syncSettings.js
+++ packages/vscode-control/src/syncSettings.js
@@ -1,41 +1,50 @@
-import { resolve } from 'path'
 import { copySync, writeJsonSync } from 'fs-extra'
-import { toDecimal, maybe } from 'rambdax'
 import { defaultTo } from 'helpers-fn'
-import settings from '../.vscode/settings.json'
+import { resolve } from 'path'
+import { maybe, toDecimal } from 'rambdax'
+
+import settings from '../.vscode/settings'
 import {
+  JS_SNIPPETS,
   KEYBINDING,
   SETTINGS,
-  JS_SNIPPETS,
   TS_SNIPPETS,
-  TSX_SNIPPETS
+  TSX_SNIPPETS,
 } from './constants'
 
-const FONT_SIZE = 14
+const FONT_SIZE = 16
 const SUGGEST_LINE_HEIGHT = 15
-const SUGGEST_FONT_SIZE = 13
-const LINE_HEIGHT = 16
-const MONO = defaultTo('MONO', false, 'onoff') 
-const MINI_MAP = defaultTo('MINI_MAP', false, 'onoff') 
-const THEME = defaultTo('THEME', `LedZeppelin`, 'default') 
+const SUGGEST_FONT_SIZE = 14
+const LINE_HEIGHT = 17
+const MONO = defaultTo(
+  'MONO', false, 'onoff'
+)
+const MINI_MAP = defaultTo(
+  'MINI_MAP', false, 'onoff'
+)
+const THEME = defaultTo(
+  'THEME', 'LedZeppelin', 'default'
+)
 
 const MODES = {
-  small: {
-    zoomScale: 0.86,
-    scale: 1.15
+  small : {
+    scale     : 1.15,
+    zoomScale : 0.86,
   },
-  normal: {
-    zoomScale: 1.06,
-    scale: 1.75
+  normal : {
+    scale     : 1.25,
+    zoomScale : 1.06,
   },
-  large: {
-    zoomScale: 1.5,
-    scale: 1.8
+  large : {
+    scale     : 1.7,
+    zoomScale : 1.5,
   },
 }
 
-const MODE_KEY = defaultTo('MODE', `normal`, 'default') 
-const MODE = MODES[MODE_KEY]
+const MODE_KEY = defaultTo(
+  'MODE', 'normal', 'default'
+)
+const MODE = MODES[ MODE_KEY ]
 
 const KEYBINDING_SOURCE = resolve(__dirname, '../.vscode/keybindings.json')
 const SNIPPETS_SOURCE = resolve(__dirname, '../.vscode/snippets.json')
@@ -64,41 +73,40 @@ const getCalculatedOptions = () => {
   const fontSize = toDecimal(FONT_SIZE * SCALE_FACTOR)
   const zoomLevel = MODE.zoomScale
   const lineHeight = toDecimal(LINE_HEIGHT * SCALE_FACTOR, 2)
-  const suggestFontSize = Math.round(toDecimal(SUGGEST_FONT_SIZE * SCALE_FACTOR, 2 ))
-  const suggestLineHeight = Math.round(toDecimal(SUGGEST_LINE_HEIGHT * SCALE_FACTOR ))
-  const terminalFontSize = Math.round(toDecimal(FONT_SIZE * (SCALE_FACTOR * 0.55) ))
+  const suggestFontSize = Math.round(toDecimal(SUGGEST_FONT_SIZE * SCALE_FACTOR, 2))
+  const suggestLineHeight = Math.round(toDecimal(SUGGEST_LINE_HEIGHT * SCALE_FACTOR))
+  const terminalFontSize = Math.round(toDecimal(FONT_SIZE * (SCALE_FACTOR * 0.55)))
 
   const fontSettings = {
-    "terminal.integrated.fontFamily": MONO? 'Operator Mono': DEFAULT_FONT,
-    'debug.console.fontFamily' : MONO? 'Operator Mono': DEFAULT_FONT,
-    "editor.fontFamily": MONO? 'Operator Mono': DEFAULT_FONT,
-    "editor.fontLigatures": !MONO 
+    'terminal.integrated.fontFamily' : MONO ? 'Operator Mono' : DEFAULT_FONT,
+    'debug.console.fontFamily'       : MONO ? 'Operator Mono' : DEFAULT_FONT,
+    'editor.fontFamily'              : MONO ? 'Operator Mono' : DEFAULT_FONT,
+    'editor.fontLigatures'           : !MONO,
   }
 
-  return  {
+  return {
     ...fontSettings,
-    "editor.fontSize": fontSize,
-    "window.zoomLevel": zoomLevel,
-    "editor.lineHeight": lineHeight,
-    "editor.suggestFontSize": suggestFontSize,
-    "editor.suggestLineHeight": suggestLineHeight,
-    "terminal.integrated.fontSize": terminalFontSize,
+    'editor.fontSize'              : fontSize,
+    'window.zoomLevel'             : zoomLevel,
+    'editor.lineHeight'            : lineHeight,
+    'editor.suggestFontSize'       : suggestFontSize,
+    'editor.suggestLineHeight'     : suggestLineHeight,
+    'terminal.integrated.fontSize' : terminalFontSize,
   }
 }
 
 function getMinimapOptions(){
   const whenTrue = {
-    "editor.minimap.enabled": true,
-    "editor.minimap.maxColumn": 40,
-    "editor.minimap.renderCharacters": false,
-    "editor.minimap.scale": 1,
-    "editor.minimap.side": "right",
-    "editor.minimap.size": "fill",
+    'editor.minimap.enabled'          : true,
+    'editor.minimap.maxColumn'        : 40,
+    'editor.minimap.renderCharacters' : false,
+    'editor.minimap.scale'            : 1,
+    'editor.minimap.side'             : 'right',
+    'editor.minimap.size'             : 'fill',
   }
-  const whenFalse = {
-    "editor.minimap.enabled": false,
-  }
-  return MINI_MAP? whenTrue : whenFalse
+  const whenFalse = { 'editor.minimap.enabled' : false }
+
+  return MINI_MAP ? whenTrue : whenFalse
 }
 
 function syncSettings(){
@@ -106,14 +114,12 @@ function syncSettings(){
     ...settings,
     ...getMinimapOptions(),
     ...getCalculatedOptions(),
-    'magicBeans.IS_VSCODE_INSIDERS': process.env.BETA ==='ON',
-    "workbench.colorTheme": THEME,
+    'magicBeans.IS_VSCODE_INSIDERS' : process.env.BETA === 'ON',
+    'workbench.colorTheme'          : THEME,
   }
 
   writeJsonSync(
-    SETTINGS,
-    newOptions,
-    { spaces : 2 }
+    SETTINGS, newOptions, { spaces : 2 }
   )
 }
 
@@ -123,10 +129,10 @@ function syncSnippets(){
   syncFiles(SNIPPETS_SOURCE, TSX_SNIPPETS)
 }
 
-void function sync(){
+void (function sync(){
   console.log('START')
   syncFiles(KEYBINDING_SOURCE, KEYBINDING)
   syncSnippets()
   syncSettings()
   console.log('END')
-}()
+})()
