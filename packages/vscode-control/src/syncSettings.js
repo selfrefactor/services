252b8b5d5442effa8985b8e57cef90622968ad18
Deyan Totev <deyan.totev.extern@seven.one>
2023-03-13 14:25:39 +0200

    chore@small

--- packages/vscode-control/src/syncSettings.js
+++ packages/vscode-control/src/syncSettings.js
@@ -234,7 +234,7 @@ function getAdditionalSettings(){
     // click to go to recent files
     'window.commandCenter'                                   : false,
     'window.titleBarStyle'                                   : 'custom',
-    'window.title'                                           : "${file}/aas",
+    'window.title'                                           : "${fileDirname}/aas",
     // without comments
     'breadcrumbs.enabled'                                    : false,
     'debug.inlineValues'                                     : 'off',
