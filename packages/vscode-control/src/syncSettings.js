6f324e35208f18d0048f59aa875bf7b667f27687
Deyan Totev <deyan.totev.extern@seven.one>
2023-06-21 15:31:04 +0300

    chore@small

--- packages/vscode-control/src/syncSettings.js
+++ packages/vscode-control/src/syncSettings.js
@@ -85,7 +85,7 @@ function getNewSettings(){
 function testNewSettings(){
   return {
     // to reduce width of action panel
-    "workbench.activityBar.width": 30,
+    "workbench.activityBar.width": 100,
     // to fix not working word wrap
     'chat.editor.wordWrap'                                        : 'on',
     'editor.wordWrapColumn'                                       : 70,
