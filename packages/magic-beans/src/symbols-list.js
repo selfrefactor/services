d9b5b17638673137c93e271042e2e158efc08b29
Deyan Totev <deyan.totev.extern@seven.one>
2023-08-01 15:11:43 +0300

    feat: symbols list

--- packages/magic-beans/src/symbols-list.js
+++ packages/magic-beans/src/symbols-list.js
@@ -1,45 +1,88 @@
-const vscode = require('vscode')
 const fs = require('fs')
-const { mapAsync } = require('rambdax')
+const vscode = require('vscode')
+const { any, last, mapAsync, remove } = require('rambdax')
+const { configAnt } = require('./ants/config')
+
+const FORBIDDEN_PATTERN = [ '.spec.', '.test.' ]
+
+const SYMBOLS_LIST_ALLOWED_EXTENSIONS = configAnt('SYMBOLS_LIST_ALLOWED_EXTENSIONS')
+
+const fileIsReportable = file => {
+  const isForbidden = any(pattern => file.path.includes(pattern),
+    FORBIDDEN_PATTERN)
+
+  return (
+    !isForbidden &&
+    any(extension => file.path.endsWith(extension),
+      SYMBOLS_LIST_ALLOWED_EXTENSIONS)
+  )
+}
 
 const HARD_LIMIT_OF_FILES_TO_PROCESS = 500
 
-async function getAllFiles() {
-  let dir = vscode.workspace.workspaceFolders[ 0 ].uri.path
-  const pattern = new vscode.RelativePattern(dir, '**/*.js')
-  const files = await vscode.workspace.findFiles(pattern, '**/node_modules/**', HARD_LIMIT_OF_FILES_TO_PROCESS)
-  
-  return files;
+async function getReportableFiles(){
+  const dir = vscode.workspace.workspaceFolders[ 0 ].uri.path
+  const pattern = new vscode.RelativePattern(dir, '**/*')
+  const files = await vscode.workspace.findFiles(
+    pattern,
+    '**/node_modules/**',
+    HARD_LIMIT_OF_FILES_TO_PROCESS
+  )
+  const filtered = files.filter(fileIsReportable)
+
+  return filtered
 }
 
-async function generateReport(files) {
-  let result = await mapAsync(async (file) => {
-    let symbols = commands.executeCommand(
-      'vscode.executeDocumentSymbolProvider',
-      file
-    )
+async function generateReport(files){
+  const dir = vscode.workspace.workspaceFolders[ 0 ].uri.path
+
+  const result = await mapAsync(async file => {
+    const symbols = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider',
+      file)
+    const details = symbols
+      .map(({ kind, name }) => ({
+        kind,
+        name,
+      }))
+      .filter(({ kind }) => kind !== 13)
+
+    const filePath = remove(dir, file.path)
+
+    return {
+      details,
+      filePath,
+    }
   }, files)
+
   return result
 }
 
-async function createTempFile() {
-  let dir = vscode.workspace.workspaceFolders[ 0 ].uri.path
-  const output = `${ __dirname }/temp.txt`
+async function showReport(reportRawData){
+  const lines = []
+  reportRawData.forEach(singleFileReport => {
+    singleFileReport.details.forEach(({ kind, name }) => {
+      lines.push(`${ name } - ${ singleFileReport.filePath } - ${ kind }`)
+    })
+  })
+  lines.sort()
+  const projectName = last(vscode.workspace.workspaceFolders[ 0 ].uri.path.split('/'))
+  const outputLocation = `${ __dirname }/symbols-list-${ projectName }.txt`
+  const output = lines.join('\n')
+  if (fs.existsSync(outputLocation))
+    fs.unlinkSync(outputLocation)
 
-  // Write data to the temporary file
-  await fs.writeFile(output, 'hello world\n');
+  fs.writeFileSync(outputLocation, Buffer.from(output, 'utf8'))
 
-  // Open the temporary file in the editor
-  const document = await vscode.workspace.openTextDocument(output);
-  await vscode.window.showTextDocument(output);
-  1
+  const uri = vscode.Uri.file(outputLocation)
+  vscode.workspace.openTextDocument(uri).then(doc => {
+    vscode.window.showTextDocument(doc)
+  })
 }
 
 async function symbolsList(){
-  1
-  await getAllFiles()
-    await createTempFile()
-  1
+  const reportableFiles = await getReportableFiles()
+  const report = await generateReport(reportableFiles)
+  await showReport(report)
 }
 
 exports.symbolsList = symbolsList
