667403b25cdd8b6a1f41550785f72fc04a324acd
Deyan Totev <deyan.totev.extern@seven.one>
2023-08-06 14:25:45 +0300

    chore@small

--- packages/vscode-control/src/syncSettings.js
+++ packages/vscode-control/src/syncSettings.js
@@ -3,7 +3,7 @@ import { defaultTo, execSafe } from 'helpers-fn'
 import { resolve } from 'path'
 import { toDecimal } from 'rambdax'
 
-import settings from '../.vscode/settings-source.json'
+import settings from '../.vscode/settings-source'
 import {
   JS_SNIPPETS,
   KEYBINDING,
@@ -27,9 +27,9 @@ const FILE_ICON_THEME = defaultTo(
 )
 
 const MODES = {
-  small  : 0.65,
-  normal : 0.77,
   large  : 1,
+  normal : 0.77,
+  small  : 0.65,
 }
 
 const MODE_KEY = defaultTo(
@@ -46,13 +46,13 @@ const FONT = VSCODE_INSIDERS ? 'JetBrains Mono' : 'PT Mono'
 const FONT_FACTOR = 1
 
 void (async function sync(){
-  if(process.env.X === 'ON'){
+  if (process.env.X === 'ON')
     return syncSettingsx()
-  }
+
   console.log('START')
   await execSafe({
-    cwd     : resolve(__dirname, '..'),
     command : 'node visualize-keybindings.js',
+    cwd     : resolve(__dirname, '..'),
   })
   syncFiles(KEYBINDING_SOURCE, KEYBINDING)
   syncSnippets()
@@ -76,13 +76,13 @@ function getPermanentSettings(){
 
 function getNewSettings(){
   return {
-    "editor.hover.height": 1200,
+    'chat.experimental.quickQuestion.enable'         : true,
+    'editor.hover.height'                            : 1200,
+    'editor.linkedEditing'                           : true,
+    'editor.pasteAs.showPasteSelector'               : 'never',
+    'editor.suggest.snippetsPreventQuickSuggestions' : false,
     'workbench.editor.tabSizing'                     : 'fixed',
     'workbench.editor.tabSizingFixedMaxWidth'        : 150,
-    'editor.suggest.snippetsPreventQuickSuggestions' : false,
-    'editor.pasteAs.showPasteSelector'               : 'never',
-    'editor.linkedEditing'                           : true,
-    'chat.experimental.quickQuestion.enable'         : true,
   }
 }
 
@@ -91,9 +91,6 @@ function getNewSettings(){
  */
 function testNewSettings(){
   return {
-    // 'editor.wordWrapColumn'                                       : 30,
-    'typescript.tsserver.experimental.enableProjectDiagnostics'   : false, // to test
-    'editor.suggest.showStatusBar'                                : false, // to test
     // to fix not working word wrap
     'chat.editor.wordWrap'                                        : 'on',
     // by default all were `peek`
@@ -102,68 +99,72 @@ function testNewSettings(){
     'editor.gotoLocation.multipleImplementations'                 : 'goto',
     'editor.gotoLocation.multipleReferences'                      : 'goto',
     'editor.gotoLocation.multipleTypeDefinitions'                 : 'goto',
+    'editor.suggest.showStatusBar'                                : false, // to test
+    'gopls'                                                       : { 'ui.semanticTokens' : true },
+    'javascript.preferences.importModuleSpecifier'                : 'relative',
+    'javascript.suggest.autoImports'                              : true,
     // to test prefered local imports
     'javascript.suggest.includeAutomaticOptionalChainCompletions' : true,
-    'javascript.suggest.autoImports'                              : true,
-    'javascript.preferences.importModuleSpecifier'                : 'relative',
+    'typescript.preferences.importModuleSpecifier'                : 'relative',
     'typescript.suggest.autoImports'                              : true,
     'typescript.suggest.includeAutomaticOptionalChainCompletions' : true,
-    'typescript.preferences.importModuleSpecifier'                : 'relative',
+    // 'editor.wordWrapColumn'                                       : 30,
+    'typescript.tsserver.experimental.enableProjectDiagnostics'   : false, // to test
   }
 }
 
 function getEditor(){
   return {
-    'editor.stickyScroll.defaultModel'         : 'indentationModel',
-    'editor.stickyScroll.enabled'              : true,
-    'editor.hover.sticky'                      : true,
-    'editor.hover.enabled'                     : true,
-    'editor.hover.delay'                       : 400,
-    'editor.scrollBeyondLastLine'              : false,
+    'editor.codeActionsOnSave' : {
+      'source.fixAll'          : false,
+      'source.organizeImports' : false,
+    },
     'editor.cursorSmoothCaretAnimation'        : 'on',
     'editor.cursorStyle'                       : 'line-thin',
     'editor.fontLigatures'                     : true,
     'editor.guides.bracketPairsHorizontal'     : false,
     'editor.guides.highlightActiveIndentation' : false,
+    'editor.hover.delay'                       : 400,
+    'editor.hover.enabled'                     : true,
+    'editor.hover.sticky'                      : true,
     'editor.lineNumbers'                       : 'interval',
     'editor.minimap.enabled'                   : false,
     'editor.mouseWheelZoom'                    : true,
     'editor.multiCursorModifier'               : 'ctrlCmd',
     'editor.parameterHints.enabled'            : false,
-    'editor.scrollbar.verticalScrollbarSize'   : 15,
-    'editor.semanticHighlighting.enabled'      : true,
-    'editor.semanticTokenColorCustomizations'  : { enabled : true },
-    'editor.smoothScrolling'                   : true,
-    'editor.tabSize'                           : 2,
-    'editor.wordWrap'                          : 'on',
-    'editor.codeActionsOnSave'                 : {
-      'source.fixAll'          : false,
-      'source.organizeImports' : false,
-    },
-    'editor.suggest.showWords'     : false,
-    'editor.suggest.localityBonus' : true,
-    'editor.quickSuggestions'      : {
+    'editor.quickSuggestions'                  : {
       comments : 'off',
-      strings  : 'off',
       other    : 'off',
+      strings  : 'off',
     },
+    'editor.scrollbar.verticalScrollbarSize'  : 15,
+    'editor.scrollBeyondLastLine'             : false,
+    'editor.semanticHighlighting.enabled'     : true,
+    'editor.semanticTokenColorCustomizations' : { enabled : true },
+    'editor.smoothScrolling'                  : true,
+    'editor.stickyScroll.defaultModel'        : 'indentationModel',
+    'editor.stickyScroll.enabled'             : true,
+    'editor.suggest.localityBonus'            : true,
+    'editor.suggest.showWords'                : false,
+    'editor.tabSize'                          : 2,
+    'editor.wordWrap'                         : 'on',
   }
 }
 
 function getWorkbench(){
   return {
-    'workbench.iconTheme'                     : FILE_ICON_THEME,
-    'workbench.editor.pinnedTabSizing'        : 'shrink',
     'workbench.activityBar.visible'           : VSCODE_INSIDERS,
-    'workbench.editor.untitled.hint'          : 'hidden',
-    'workbench.editor.untitled.labelFormat'   : 'name',
-    'workbench.list.smoothScrolling'          : true,
-    'workbench.sideBar.location'              : 'right',
-    'workbench.startupEditor'                 : 'none',
     'workbench.editor.languageDetectionHints' : {
-      untitledEditors : false,
       notebookEditors : false,
+      untitledEditors : false,
     },
+    'workbench.editor.pinnedTabSizing'      : 'shrink',
+    'workbench.editor.untitled.hint'        : 'hidden',
+    'workbench.editor.untitled.labelFormat' : 'name',
+    'workbench.iconTheme'                   : FILE_ICON_THEME,
+    'workbench.list.smoothScrolling'        : true,
+    'workbench.sideBar.location'            : 'right',
+    'workbench.startupEditor'               : 'none',
   }
 }
 
@@ -172,20 +173,20 @@ function getExplorer(){
     'explorer.confirmDelete'         : false,
     'explorer.confirmDragAndDrop'    : false,
     'explorer.incrementalNaming'     : 'smart',
-    'explorer.sortOrder'             : 'default',
     'explorer.openEditors.sortOrder' : 'alphabetical',
+    'explorer.sortOrder'             : 'default',
   }
 }
 
 function getGit(){
   return {
+    'git.allowNoVerifyCommit'           : true,
     'git.confirmEmptyCommits'           : true,
     'git.confirmForcePush'              : true,
     'git.confirmNoVerifyCommit'         : true,
     'git.confirmSync'                   : false,
-    'git.promptToSaveFilesBeforeCommit' : 'never',
-    'git.allowNoVerifyCommit'           : true,
     'git.fetchOnPull'                   : true,
+    'git.promptToSaveFilesBeforeCommit' : 'never',
   }
 }
 
@@ -194,10 +195,10 @@ function getImportantSettings(){
     'gitlab.customQueries' : [
       {
         name       : 'All project merge requests',
-        type       : 'merge_requests',
+        noItemText : 'The project has no merge requests',
         scope      : 'all',
         state      : 'opened',
-        noItemText : 'The project has no merge requests',
+        type       : 'merge_requests',
       },
     ],
   }
@@ -205,62 +206,56 @@ function getImportantSettings(){
 
 function getAdditionalSettings(){
   return {
-    'window.menuBarVisibility'                               : 'toggle',
-    'editor.tabCompletion'                                   : 'on',
-    'zenMode.hideTabs'                                       : false,
+    // without comments
+    'breadcrumbs.enabled'         : false,
+    'debug.inlineValues'          : 'off',
+    'debug.javascript.usePreview' : true,
+    'diffEditor.diffAlgorithm'    : 'advanced',
+    'editor.colorDecoratorsLimit' : 10,
+    'editor.scrollbar.vertical'   : 'visible',
+    'editor.tabCompletion'        : 'on',
+    'files.enableTrash'           : false,
+    'files.exclude'               : {
+      '.cache'            : true,
+      '**/.awcache'       : true,
+      '**/.cache-loader'  : true,
+      '**/.DS_Store'      : false,
+      '**/.git'           : true,
+      '**/.hg'            : false,
+      '**/.idea'          : true,
+      '**/.svn'           : false,
+      '**/coverage'       : true,
+      '**/coverage-ts'    : true,
+      '**/CVS'            : false,
+      '**/node_modules'   : true,
+      '**/yarn-error.log' : true,
+      'dev_dist'          : true,
+      'dist'              : false,
+      'docs'              : false,
+      'yarn.lock'         : true,
+    },
+    'files.hotExit'                     : 'off',
+    'git.autofetch'                     : true,
+    'git.openRepositoryInParentFolders' : 'always',
+    'github.copilot.enable'             : {
+      '*'         : true,
+      'markdown'  : true,
+      'plaintext' : true,
+      'yaml'      : true,
+    },
     'gitlab.showProjectMergeRequests'                        : false,
-    'editor.colorDecoratorsLimit'                            : 10,
-    'editor.scrollbar.vertical'                              : 'visible',
-    'git.openRepositoryInParentFolders'                      : 'always',
+    'go.alternateTools'                                      : { go : '/Users/user/.local/share/rtx/installs/golang/1.20/go/bin' },
+    'go.gopath'                                              : '/Users/user/.local/share/rtx/installs/golang/1.20/go/bin',
     'javascript.inlayHints.enumMemberValues.enabled'         : false,
     'javascript.inlayHints.functionLikeReturnTypes.enabled'  : false,
     'javascript.inlayHints.parameterTypes.enabled'           : false,
     'javascript.inlayHints.propertyDeclarationTypes.enabled' : false,
     'javascript.inlayHints.variableTypes.enabled'            : false,
-    'typescript.inlayHints.enumMemberValues.enabled'         : false,
-    'typescript.inlayHints.functionLikeReturnTypes.enabled'  : false,
-    'typescript.inlayHints.parameterTypes.enabled'           : false,
-    'typescript.inlayHints.variableTypes.enabled'            : false,
-    // click to go to recent files
-    'window.commandCenter'                                   : false,
-    'window.titleBarStyle'                                   : 'custom',
-    'window.title'                                           : '${dirty}${activeEditorMedium}',
-    // without comments
-    'breadcrumbs.enabled'                                    : false,
-    'debug.inlineValues'                                     : 'off',
-    'debug.javascript.usePreview'                            : true,
-    'files.enableTrash'                                      : false,
-    'files.hotExit'                                          : 'off',
-    'git.autofetch'                                          : true,
     'javascript.updateImportsOnFileMove.enabled'             : 'always',
     'js/ts.implicitProjectConfig.checkJs'                    : true,
     'json.format.enable'                                     : false,
     'json.maxItemsComputed'                                  : 1000,
-    'npm.autoDetect'                                         : 'off',
-    'npm.packageManager'                                     : 'yarn',
-    // as it doesn't work; maybe due to Copilot interfering
-    'npm.fetchOnlinePackageInfo'                             : false,
-    'scm.defaultViewMode'                                    : 'tree',
-    'search.collapseResults'                                 : 'alwaysCollapse',
-    'search.seedOnFocus'                                     : false,
-    'search.smartCase'                                       : true,
-    'search.useReplacePreview'                               : false,
-    'task.autoDetect'                                        : 'off',
-    'task.quickOpen.detail'                                  : false,
-    'telemetry.enableTelemetry'                              : false,
-    'telemetry.telemetryLevel'                               : 'off',
-    'terminal.integrated.gpuAcceleration'                    : 'off',
-    'typescript.updateImportsOnFileMove.enabled'             : 'always',
-    'update.mode'                                            : 'none',
-    'zenMode.restore'                                        : false,
-    'diffEditor.diffAlgorithm'                               : 'advanced',
-    'github.copilot.enable'                                  : {
-      '*'         : true,
-      'yaml'      : true,
-      'plaintext' : true,
-      'markdown'  : true,
-    },
-    'magicBeans.RANDOM_FILE_ALLOWED_EXTENSIONS' : [
+    'magicBeans.RANDOM_FILE_ALLOWED_EXTENSIONS'              : [
       '.css',
       '.feature',
       '.go',
@@ -294,45 +289,51 @@ function getAdditionalSettings(){
       'pre-commit',
       'setup.py',
     ],
-    'go.gopath'         : '/Users/user/.local/share/rtx/installs/golang/1.20/go/bin',
-    'go.alternateTools' : { go : '/Users/user/.local/share/rtx/installs/golang/1.20/go/bin' },
-    'files.exclude'     : {
-      '.cache'            : true,
-      '**/.awcache'       : true,
-      '**/.cache-loader'  : true,
-      '**/.DS_Store'      : false,
-      '**/.git'           : true,
-      '**/.idea'          : true,
-      '**/.hg'            : false,
-      '**/.svn'           : false,
-      '**/coverage'       : true,
-      '**/coverage-ts'    : true,
-      '**/CVS'            : false,
-      '**/node_modules'   : true,
-      '**/yarn-error.log' : true,
-      'dev_dist'          : true,
-      'dist'              : false,
-      'docs'              : false,
-      'yarn.lock'         : true,
-    },
+    'npm.autoDetect'                                        : 'off',
+    // as it doesn't work; maybe due to Copilot interfering
+    'npm.fetchOnlinePackageInfo'                            : false,
+    'npm.packageManager'                                    : 'yarn',
+    'scm.defaultViewMode'                                   : 'tree',
+    'search.collapseResults'                                : 'alwaysCollapse',
+    'search.seedOnFocus'                                    : false,
+    'search.smartCase'                                      : true,
+    'search.useReplacePreview'                              : false,
+    'task.autoDetect'                                       : 'off',
+    'task.quickOpen.detail'                                 : false,
+    'telemetry.enableTelemetry'                             : false,
+    'telemetry.telemetryLevel'                              : 'off',
+    'terminal.integrated.gpuAcceleration'                   : 'off',
+    'typescript.inlayHints.enumMemberValues.enabled'        : false,
+    'typescript.inlayHints.functionLikeReturnTypes.enabled' : false,
+    'typescript.inlayHints.parameterTypes.enabled'          : false,
+    'typescript.inlayHints.variableTypes.enabled'           : false,
+    'typescript.updateImportsOnFileMove.enabled'            : 'always',
+    'update.mode'                                           : 'none',
+    // click to go to recent files
+    'window.commandCenter'                                  : false,
+    'window.menuBarVisibility'                              : 'toggle',
+    'window.title'                                          : '${dirty}${activeEditorMedium}',
+    'window.titleBarStyle'                                  : 'custom',
+    'zenMode.hideTabs'                                      : false,
+    'zenMode.restore'                                       : false,
   }
 }
 
 function getSpellingSettings(){
   return {
-    'cSpell.allowCompoundWords'  : false,
-    'cSpell.maxNumberOfProblems' : 3,
-    'cSpell.minWordLength'       : 4,
-    'cSpell.numSuggestions'      : 3,
-    'cSpell.showStatus'          : false,
-    'cSpell.spellCheckDelayMs'   : 200,
-    'cSpell.ignorePaths'         : [
+    'cSpell.allowCompoundWords' : false,
+    'cSpell.ignorePaths'        : [
       'node_modules', // this will ignore anything the node_modules directory
       '.git', // Ignore the .git directory
       'countries.json',
       'languages.json',
       'introspections.json',
     ],
+    'cSpell.maxNumberOfProblems' : 3,
+    'cSpell.minWordLength'       : 4,
+    'cSpell.numSuggestions'      : 3,
+    'cSpell.showStatus'          : false,
+    'cSpell.spellCheckDelayMs'   : 200,
   }
 }
 
@@ -349,20 +350,20 @@ function getCalculatedOptions(){
   const terminalFontSize = Math.round(toDecimal(FONT_SIZE * (SCALE_FACTOR * 0.65)))
 
   const fontSettings = {
-    'terminal.integrated.fontFamily' : FONT,
     'debug.console.fontFamily'       : FONT,
     'editor.fontFamily'              : FONT,
     'editor.fontLigatures'           : true,
+    'terminal.integrated.fontFamily' : FONT,
   }
 
   return {
     ...fontSettings,
     'editor.fontSize'              : fontSize,
-    'window.zoomLevel'             : 2,
     'editor.lineHeight'            : lineHeight,
     'editor.suggestFontSize'       : suggestFontSize,
     'editor.suggestLineHeight'     : suggestLineHeight,
     'terminal.integrated.fontSize' : terminalFontSize,
+    'window.zoomLevel'             : 2,
   }
 }
 
@@ -372,8 +373,8 @@ function syncSettings(){
     ...getPermanentSettings(),
     ...getCalculatedOptions(),
     'magicBeans.IS_VSCODE_INSIDERS'  : VSCODE_INSIDERS,
-    'workbench.editor.enablePreview' : !VSCODE_INSIDERS,
     'workbench.colorTheme'           : THEME,
+    'workbench.editor.enablePreview' : !VSCODE_INSIDERS,
   }
 
   writeJsonSync(
@@ -386,7 +387,7 @@ function syncSettings(){
 
 function syncSettingsx(){
   const settings = readJsonSync(SETTINGS_REFERENCE_OUTPUT)
-    
+
   writeJsonSync(
     SETTINGS, settings, { spaces : 2 }
   )
