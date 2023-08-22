cc5660369c7e8c690f23b2c65d1ea6ce019d11fb
Deyan Totev <deyan.totev.extern@seven.one>
2023-08-22 23:12:05 +0300

    chore@small

--- packages/run-fn/upgrade.sh
+++ packages/run-fn/upgrade.sh
@@ -1,1 +1,1 @@
-npm i -g run-fn&&npm uni -g vsce&&npm uni -g now&&npm uni -g @angular/cli&&npm i -g vsce&&npm i -g now&&npm i -g @angular/cli
+npm i -g run-fn && npm uni -g vsce && npm uni -g now && npm i -g vsce && npm i -g now
