98e1f8e6df17d9a9b02614211acab34cd96bf35e
Deyan Totev <deyan.totev.extern@seven.one>
2023-11-25 19:31:00 +0200

    chore@small

--- packages/magic-beans/src/randomFile.js
+++ packages/magic-beans/src/randomFile.js
@@ -1,25 +1,21 @@
 const vscode = require('vscode')
 const {
-  delay,
   getter,
   removeIndex,
   setter,
   shuffle,
-  waitFor,
 } = require('rambdax')
 const { configAnt } = require('./ants/config')
 const { logToUser } = require('./bar')
 const { REQUEST_RANDOM_FILE } = require('./constants')
 const { scanFolder } = require('helpers-fn')
 
-const RANDOM_FILE_SKIP_PATTERNS = configAnt('RANDOM_FILE_SKIP_PATTERNS')
+const RANDOM_FILE_SKIP_DIRECTORIES = configAnt('RANDOM_FILE_SKIP_DIRECTORIES')
 const RANDOM_FILE_ALLOWED = configAnt('RANDOM_FILE_ALLOWED')
-const RANDOM_FILE_FORBIDDEN_EXTENSIONS = configAnt('RANDOM_FILE_FORBIDDEN_EXTENSIONS')
+const RANDOM_FILE_FORBIDDEN = configAnt('RANDOM_FILE_FORBIDDEN')
 
 
 function changeOpenedFile(filePath){
-  // editor should have
-  // "workbench.editor.enablePreview": true,
   const openPath = vscode.Uri.file(filePath)
   vscode.workspace.openTextDocument(openPath).then(doc => {
     vscode.window.showTextDocument(doc)
@@ -29,16 +25,16 @@ function changeOpenedFile(filePath){
 async function randomFileInitialize(){
   const projectFolder = vscode.workspace.workspaceFolders[ 0 ].uri.path
   const files = await scanFolder({
-    excludeFn : dir => RANDOM_FILE_SKIP_PATTERNS.includes(dir),
+    excludeFn : dir => RANDOM_FILE_SKIP_DIRECTORIES.includes(dir),
     filterFn  : filePath => {
       const [ passAllowed ] = RANDOM_FILE_ALLOWED.filter(singleExtension => filePath.includes(singleExtension))
       if (!passAllowed) return false
 
-      const [ failForbiddenExtension ] =
-        RANDOM_FILE_FORBIDDEN_EXTENSIONS.filter(singleExtension =>
-          filePath.endsWith(singleExtension))
+      const [ failForbidden ] =
+        RANDOM_FILE_FORBIDDEN.filter(singleExtension =>
+          filePath.includes(singleExtension))
 
-      return !failForbiddenExtension
+      return !failForbidden
     },
     folder   : projectFolder,
     maxDepth : 20,
