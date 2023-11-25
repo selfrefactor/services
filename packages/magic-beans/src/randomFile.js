3638f95ab6108cbdfd242d4ee1194680bfb5d489
Deyan Totev <deyan.totev.extern@seven.one>
2023-11-25 10:34:25 +0200

    chore@small

--- packages/magic-beans/src/randomFile.js
+++ packages/magic-beans/src/randomFile.js
@@ -13,54 +13,16 @@ const { REQUEST_RANDOM_FILE } = require('./constants')
 const { scanFolder } = require('helpers-fn')
 
 const RANDOM_FILE_SKIP_PATTERNS = configAnt('RANDOM_FILE_SKIP_PATTERNS')
-const RANDOM_FILE_ALLOWED_EXTENSIONS = configAnt('RANDOM_FILE_ALLOWED_EXTENSIONS')
+const RANDOM_FILE_ALLOWED = configAnt('RANDOM_FILE_ALLOWED')
 const RANDOM_FILE_FORBIDDEN_EXTENSIONS = configAnt('RANDOM_FILE_FORBIDDEN_EXTENSIONS')
-const RANDOM_FILE_PERIOD = configAnt('RANDOM_FILE_PERIOD')
 
-const PREFER_PACKAGE_JSON = false
 
-class Delay{
-  constructor(){
-    this.timeout = null
-    this.actionIsApplied = false
-  }
-
-  actionApplied(){
-    this.actionIsApplied = true
-  }
-
-  clear(){
-    if (this.timeout) clearTimeout(this.timeout)
-  }
-
-  delay(ms){
-    return new Promise(resolve => {
-      this.timeout = setTimeout(() => {
-        resolve()
-      }, ms)
-    })
-  }
-
-  async startWaitingForAction(ms){
-    this.actionIsApplied = false
-    let timestamp = Date.now()
-    await waitFor(
-      () => this.actionIsApplied === true, ms, 100
-    )()
-
-    if (this.actionIsApplied === true) return Date.now() - timestamp
-  }
-}
-
-const delayInstance = new Delay()
-
-function changeOpenedFile(filePath, callback = () => {}){
+function changeOpenedFile(filePath){
   // editor should have
   // "workbench.editor.enablePreview": true,
   const openPath = vscode.Uri.file(filePath)
   vscode.workspace.openTextDocument(openPath).then(doc => {
     vscode.window.showTextDocument(doc)
-    delay(250).then(callback)
   })
 }
 
@@ -69,8 +31,8 @@ async function randomFileInitialize(){
   const files = await scanFolder({
     excludeFn : dir => RANDOM_FILE_SKIP_PATTERNS.includes(dir),
     filterFn  : filePath => {
-      const [ passAllowedExtension ] = RANDOM_FILE_ALLOWED_EXTENSIONS.filter(singleExtension => filePath.endsWith(singleExtension))
-      if (!passAllowedExtension) return false
+      const [ passAllowed ] = RANDOM_FILE_ALLOWED.filter(singleExtension => filePath.includes(singleExtension))
+      if (!passAllowed) return false
 
       const [ failForbiddenExtension ] =
         RANDOM_FILE_FORBIDDEN_EXTENSIONS.filter(singleExtension =>
@@ -88,14 +50,6 @@ async function randomFileInitialize(){
     return
   }
   const randomized = shuffle(files)
-  if (PREFER_PACKAGE_JSON)
-    randomized.sort((a, b) => {
-      if (a.includes('package.json')) return -1
-      if (b.includes('package.json')) return 1
-
-      return 0
-    })
-
   setter('files', randomized)
 }
 
@@ -110,7 +64,7 @@ function requestRandomFile(){
   const index = 0
   changeOpenedFile(files[ index ])
   setter('files', removeIndex(index, files))
-  logToUser(`${ files.length - 1 } files left`)
+  logToUser(`${ files.length - 1 } files left | Magic beans random file mode`)
 
   return true
 }
@@ -121,38 +75,6 @@ async function requestRandomFileFn(){
     setter(REQUEST_RANDOM_FILE, true)
   }
   requestRandomFile()
-  delayInstance.actionApplied()
-}
-
-async function startAutomatedMode(){
-  requestRandomFile()
-  while (true){
-    // wait either for user to request a file or for the period to pass
-    const result = await Promise.race([
-      delay(RANDOM_FILE_PERIOD),
-      delayInstance.delay(RANDOM_FILE_PERIOD),
-      delayInstance.startWaitingForAction(RANDOM_FILE_PERIOD),
-    ])
-    if (typeof result === 'number'){
-
-    }
-    const success = requestRandomFile()
-    if (!success) await randomFileInitialize()
-  }
-}
-
-async function requestRandomFileAutomatedFn(){
-  // so it doesn't matter which action is triggered as long as init process is done
-  if (getter(REQUEST_RANDOM_FILE)){
-    requestRandomFile()
-    delayInstance.actionApplied()
-
-    return
-  }
-  await randomFileInitialize()
-  setter(REQUEST_RANDOM_FILE, true)
-  startAutomatedMode()
 }
 
 exports.requestRandomFile = requestRandomFileFn
-exports.requestRandomFileAutomated = requestRandomFileAutomatedFn
--- packages/magic-beans/src/symbols-list.js
+++ packages/magic-beans/src/symbols-list.js
@@ -4,10 +4,15 @@ const { any, last, mapAsync, remove } = require('rambdax')
 const { configAnt } = require('./ants/config')
 
 const FORBIDDEN_PATTERN = [ '.spec.', '.test.' ]
-
+const DEBUG_MODE = true
+const DEBUG_PATTERN =
+  'src/handlers/aggregation/processors/parent-processor.ts'
 const SYMBOLS_LIST_ALLOWED_EXTENSIONS = configAnt('SYMBOLS_LIST_ALLOWED_EXTENSIONS')
 
 const fileIsReportable = file => {
+  if (DEBUG_MODE)
+    return file.path.includes(DEBUG_PATTERN)
+
   const isForbidden = any(pattern => file.path.includes(pattern),
     FORBIDDEN_PATTERN)
 
@@ -39,7 +44,7 @@ async function generateReport(files){
   const result = await mapAsync(async file => {
     const symbols = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider',
       file)
-    if(!symbols) return false
+    if (!symbols) return false
 
     const details = symbols
       .map(({ kind, name }) => ({
@@ -47,6 +52,12 @@ async function generateReport(files){
         name,
       }))
       .filter(({ kind }) => kind !== 13)
+    const detailsx = symbols
+      .map(({ kind, name }) => ({
+        kind,
+        name,
+      }))
+      .filter(({ kind }) => kind === 13)
 
     const filePath = remove(dir, file.path)
 
@@ -70,8 +81,7 @@ async function showReport(reportRawData){
   const projectName = last(vscode.workspace.workspaceFolders[ 0 ].uri.path.split('/'))
   const outputLocation = `${ __dirname }/symbols-list-${ projectName }.txt`
   const output = lines.join('\n')
-  if (fs.existsSync(outputLocation))
-    fs.unlinkSync(outputLocation)
+  if (fs.existsSync(outputLocation)) fs.unlinkSync(outputLocation)
 
   fs.writeFileSync(outputLocation, Buffer.from(output, 'utf8'))
 
@@ -86,7 +96,7 @@ async function symbolsList(){
     const reportableFiles = await getReportableFiles()
     const report = await generateReport(reportableFiles)
     await showReport(report)
-  }catch (e){
+  } catch (e){
     console.log(e)
   }
 }
