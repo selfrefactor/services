677fb7f29cd616f4c7c695c77db833cd69991164
Deyan Totev <deyan.totev.extern@seven.one>
2024-01-31 21:06:39 +0200

    chore@small

--- packages/magic-beans/src/randomFile.js
+++ packages/magic-beans/src/randomFile.js
@@ -1,10 +1,5 @@
 const vscode = require('vscode')
-const {
-  getter,
-  removeIndex,
-  setter,
-  shuffle,
-} = require('rambdax')
+const { getter, removeIndex, setter, shuffle } = require('rambdax')
 const { configAnt } = require('./ants/config')
 const { logToUser } = require('./bar')
 const { REQUEST_RANDOM_FILE } = require('./constants')
@@ -13,35 +8,45 @@ const { scanFolder } = require('helpers-fn')
 const RANDOM_FILE_SKIP_DIRECTORIES = configAnt('RANDOM_FILE_SKIP_DIRECTORIES')
 const RANDOM_FILE_ALLOWED = configAnt('RANDOM_FILE_ALLOWED')
 const RANDOM_FILE_FORBIDDEN = configAnt('RANDOM_FILE_FORBIDDEN')
-const RANDOM_FILE_ALLOWED_DIRECTORY = configAnt('RANDOM_FILE_ALLOWED_DIRECTORY')
+const RANDOM_FILE_ALLOWED_DIRECTORY = configAnt(
+  'RANDOM_FILE_ALLOWED_DIRECTORY',
+)
 
-function changeOpenedFile(filePath){
+function changeOpenedFile(filePath) {
   const openPath = vscode.Uri.file(filePath)
-  vscode.workspace.openTextDocument(openPath).then(doc => {
+  vscode.workspace.openTextDocument(openPath).then((doc) => {
     vscode.window.showTextDocument(doc)
   })
 }
 
-async function randomFileInitialize(){
-  const projectFolder = vscode.workspace.workspaceFolders[ 0 ].uri.path
+async function randomFileInitialize() {
+  const projectFolder = vscode.workspace.workspaceFolders[0].uri.path
+  const list = []
   const files = await scanFolder({
-    excludeFn : dir => RANDOM_FILE_SKIP_DIRECTORIES.includes(dir),
-    filterFn  : filePath => {
-      const [ passAllowed ] = RANDOM_FILE_ALLOWED.filter(singleExtension => filePath.includes(singleExtension))
+    excludeFn: dir => RANDOM_FILE_SKIP_DIRECTORIES.includes(dir),
+    filterFn: (filePath) => {
+      const [passAllowed] = RANDOM_FILE_ALLOWED.filter(singleExtension =>
+        filePath.endsWith(singleExtension),
+      )
       if (!passAllowed) return false
+      const allowedDirectory = RANDOM_FILE_ALLOWED_DIRECTORY
+        ? filePath.includes(RANDOM_FILE_ALLOWED_DIRECTORY)
+        : undefined
 
-      const [ failForbidden ] =
-        RANDOM_FILE_FORBIDDEN.filter(singleExtension =>
-          filePath.includes(singleExtension))
-      if( !failForbidden ) return true
-      if( !RANDOM_FILE_ALLOWED_DIRECTORY ) return false
-      return filePath.includes(RANDOM_FILE_ALLOWED_DIRECTORY)
+      const [failForbidden] = RANDOM_FILE_FORBIDDEN.filter(singleExtension =>
+        filePath.endsWiths(singleExtension),
+      )
+      if (!failForbidden) {
+        list.push(filePath)
+      }
+
+      return allowedDirectory && !failForbidden
     },
-    folder   : projectFolder,
-    maxDepth : 20,
+    folder: projectFolder,
+    maxDepth: 20,
   })
 
-  if (files.length === 0){
+  if (files.length === 0) {
     logToUser('no files left')
 
     return
@@ -50,24 +55,24 @@ async function randomFileInitialize(){
   setter('files', randomized)
 }
 
-function requestRandomFile(){
+function requestRandomFile() {
   const files = getter('files')
-  if (!files){
+  if (!files) {
     logToUser('no files')
 
     return false
   }
   if (files.length === 0) return false
   const index = 0
-  changeOpenedFile(files[ index ])
+  changeOpenedFile(files[index])
   setter('files', removeIndex(index, files))
-  logToUser(`${ files.length - 1 } files left | Magic beans random file mode`)
+  logToUser(`${files.length - 1} files left | Magic beans random file mode`)
 
   return true
 }
 
-async function requestRandomFileFn(){
-  if (!getter(REQUEST_RANDOM_FILE)){
+async function requestRandomFileFn() {
+  if (!getter(REQUEST_RANDOM_FILE)) {
     await randomFileInitialize()
     setter(REQUEST_RANDOM_FILE, true)
   }
