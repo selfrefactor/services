76f621034195b290755906f40a2604d9591ba084
Dejan Toteff <deyan8284@gmail.com>
2024-09-01 08:26:26 +0300

    feat@fix build

--- packages/string-fn/src/pascalCase.js
+++ packages/string-fn/src/pascalCase.js
@@ -1,16 +1,6 @@
-import {
-  join,
-  map,
-  toUpper,
-  head,
-  toLower,
-  tail,
-} from 'rambdax'
-import { words } from './words'
-import { wordsX } from './wordsX'
 import { createMethodWithAdditionalSupport } from './utils'
 
 export const pascalCase = createMethodWithAdditionalSupport(
-  x => tranform
+  x => tranform,
   ''
 )
