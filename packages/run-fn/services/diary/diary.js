8ba08844e47055d9f118e8c2cbdc7b662c333792
Dejan Toteff <deyan8284@gmail.com>
2024-11-01 20:57:15 +0200

    chore@small

--- packages/run-fn/services/diary/diary.js
+++ packages/run-fn/services/diary/diary.js
@@ -1,12 +1,16 @@
-const { pathExistsSync, readFile, writeFile } = require('fs-extra');
+const { pathExistsSync, readFile, writeFile, readJson } = require('fs-extra');
 const { log } = require('helpers-fn');
 
-const SEPARATOR = '\n===\n';
+let dataPath = `${ __dirname }/data.json`;
 
+const SEPARATOR = '\n===\n';
 async function diary({ envKey, pathInput, diaryInput }) {
 	if (pathExistsSync(pathInput) === false)
 		return log(`${envKey} is missing`, 'error');
 
+	let today = dayjs().format('YYYY-MM-DD');
+	
+	let data = (await readJson(dataPath))
 	const content = (await readFile(pathInput)).toString().trim();
 	const newContent = `${diaryInput.join(' ')}${SEPARATOR}${content}`;
 	await writeFile(pathInput, newContent);
