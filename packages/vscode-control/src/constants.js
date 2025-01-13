e1585497bf09888d984bb1e6a0a87742982a4e57
Dejan Toteff <deyan8284@gmail.com>
2025-01-13 17:34:17 +0200

    chore@small

--- packages/vscode-control/src/constants.js
+++ packages/vscode-control/src/constants.js
@@ -5,12 +5,14 @@ const BASE_BETA = `${process.env.HOME}/.config/Code - Insiders/User`
 const BASE = process.env.BETA === 'ON' ? BASE_BETA :BASE_STABLE
 
 let KEYBINDING = `${BASE}/keybindings.json`
-exports.SETTINGS = `${BASE}/settings.json`
+let SETTINGS = `${BASE}/settings.json`
+
+exports.SETTINGS = SETTINGS
 exports.JS_SNIPPETS = `${BASE}/snippets/javascript.json`
 exports.JSX_SNIPPETS = `${BASE}/snippets/javascriptreact.json`
 exports.TS_SNIPPETS = `${BASE}/snippets/typescript.json`
 exports.TSX_SNIPPETS = `${BASE}/snippets/typescriptreact.json`
 
 exports.KEYBINDING = KEYBINDING
-let editorExists = pathExistsSync(KEYBINDING)
+let editorExists = pathExistsSync(SETTINGS)
 exports.editorExists = editorExists
