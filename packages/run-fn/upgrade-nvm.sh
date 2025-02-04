15afafa1f377ff1c93a1d1124fd796a73d68de62
Dejan Toteff <deyan8284@gmail.com>
2025-02-04 15:32:20 +0200

    chore@small

--- packages/run-fn/upgrade-nvm.sh
+++ packages/run-fn/upgrade-nvm.sh
@@ -1,1 +1,1 @@
-npm uni -g run-fn && npm uni -g serve && npm uni -g @vscode/vsce && npm uni -g vercel && npm i -g @vscode/vsce && npm i -g vercel && npm i -g run-fn && npm i -g serve
+npm i -g @vscode/vsce && npm i -g vercel && npm i -g run-fn && npm i -g serve && npm i -g ovsx
