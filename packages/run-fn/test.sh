88666c1e1fac6b4e00778ebf82710c87d7042e73
Dejan Toteff <deyan8284@gmail.com>
2025-02-06 21:32:05 +0200

    chore@small

--- packages/run-fn/test.sh
+++ packages/run-fn/test.sh
@@ -2,4 +2,4 @@
 # node bin/run.js diary a b c
 # RUN_FN_CWD=/home/we/repos/services/packages/string-fn/src node bin/run.js lint:folder 
 # RUN_FN_CWD=/home/we/repos/starters/typescript-starter node bin/run.js dep:next 9
-RUN_FN_CWD=/home/we/repos/services/packages/run-fn/services node bin/run.js lint:folder
+RUN_FN_CWD=/home/we/repos/services/packages/run-fn/services node bin/run.js lint:folder > foo.txt
